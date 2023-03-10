18e941a540c024ac13c9ac3e92d6c1a7bed9962a
somnath07 <123575623+bsomnath07@users.noreply.github.com>
2023-03-10 18:30:50 +0530

    message here

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import Heading from "./components/Heading";
+import SubHeading from "./components/SubHeading";
+import InputQuery from "./components/InputQuery";
+import SubmitButton from "./components/SubmitButton";
+import "./styles/App.css";
 
-const App = () => {
+function App() {
+  const [query, setQuery] = useState("");
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Query submitted: ${query}`);
+  };
+
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <form onSubmit={handleFormSubmit}>
+        <InputQuery value={query} onChange={handleQueryChange} />
+        <SubmitButton />
+      </form>
     </div>
-  )
+  );
 }
+
+export default App;
 
 
